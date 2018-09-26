05ab4ce3f81a21bd0e4b37cabe50d2405cf707f4
a4163581 <31943609+a4163581@users.noreply.github.com>
2018-09-26 12:56:21 +0800

    Update app.js

--- exercise/hw3/app.js
+++ exercise/hw3/app.js
@@ -2,13 +2,17 @@ const Koa = require('koa');
 const app = module.exports = new Koa();
 
 app.use(async function(ctx) {
+  console.log('url=', ctx.url)
   // console.log('url=', ctx.url)
   switch (ctx.url) {
     case '/hello': ctx.body = '你好'; break
+    case '/name': ctx.body = 'neil'; break
     // '/name' : ...; break
+    case '/id': ctx.body = '110510551'; break
     // '/id'   : ...; break
     default : ctx.status = 404
   }
+  ctx.body = 'Hello World';
   // ctx.body = 'Hello World';
 });
 
